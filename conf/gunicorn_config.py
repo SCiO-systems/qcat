47828e4fe051fde48a9993295a342c664682a3b7
Angelos Naoum <angelosnaoum@gmail.com>
2022-01-10 14:33:14 +0000

    dockerization update

--- conf/gunicorn_config.py
+++ conf/gunicorn_config.py
@@ -1,4 +1,5 @@
 command = '/usr/local/bin/gunicorn'
-pythonpath = '/code/apps' # container path
+#pythonpath = '/code/apps' # container path
+pythonpath = '/app/apps' # container path
 bind = '0.0.0.0:8000'
 workers = 3
