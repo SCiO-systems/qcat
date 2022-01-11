3769fedeeaf3c97ce9639911ee1b70d2a0ea83e8
Angelos Naoum <angelosnaoum@gmail.com>
2022-01-11 11:27:54 +0000

    redis authentication & gunicorn improvements

--- conf/gunicorn_config.py
+++ conf/gunicorn_config.py
@@ -1,5 +1,12 @@
 command = '/usr/local/bin/gunicorn'
-#pythonpath = '/code/apps' # container path
+#pythonpath = '/code/apps' # host path
 pythonpath = '/app/apps' # container path
 bind = '0.0.0.0:8000'
 workers = 3
+
+timeout = 999
+
+errorlog = '-'
+loglevel = 'debug'
+accesslog = '-'
+access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
